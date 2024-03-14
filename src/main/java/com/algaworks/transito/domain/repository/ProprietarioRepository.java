7099826bc15b082d9efa27b990effbdd7171f95d
josevaldojunior <jjrsmedeiros@gmail.com>
2024-03-13 21:18:03 -0300

    refactor: refatorando repositorys

--- src/main/java/com/algaworks/transito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/algaworks/transito/domain/repository/ProprietarioRepository.java
@@ -4,14 +4,9 @@ import com.algaworks.transito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
-
-    List<Proprietario> findByNomeContaining(String nome);
-
     Optional<Proprietario> findByEmail(String email);
-
 }
--- src/main/java/com/algaworks/transito/domain/repository/VeiculoRepository.java
+++ src/main/java/com/algaworks/transito/domain/repository/VeiculoRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
-
     Optional<Veiculo> findByPlaca(String placa);
-
 }
